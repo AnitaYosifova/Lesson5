ecf44d81f994ea03fa0385355cb77433748c1afe
AnitaYosifova <147909366+AnitaYosifova@users.noreply.github.com>
2023-10-22 11:35:19 +0300

    Update StudentGroup.java
    метод printStudentsInGroup() - Изкарва информация (Име, успех, ...)за
    всички студент в курса.

--- StudentGroup.java
+++ StudentGroup.java
@@ -1,26 +1,52 @@
-import java.util.Scanner;
-
 public class StudentGroup {
-    String groupSubject;
-    Student[] students;
-    int freePlaces;
-
-    Scanner sc = new Scanner(System.in);
-    int  amountOfGroupPlaces = 5;
-
-
-        StudentGroup() {
-        this.freePlaces=5;
-
-
-    StudentGroup(String groupSubject) {
-        this();
-        System.out.println("There is a new student group - " + groupSubject + "!");
-        this.groupSubject = groupSubject;
-    }
-    public void emptyGroup() {
-        students = new Student[amountOfGroupPlaces];
-        freePlaces = amountOfGroupPlaces;
-    }
-
-    }
+	String subject;
+	Student[] students;
+	int freePlaces;
+	
+	StudentGroup() {
+		this.students = new Student[5];
+		freePlaces = 5;
+	}
+	
+	StudentGroup(String subject) {
+		this();
+		this.subject = subject;
+	}
+	
+	void addStudent(Student s) {
+		if(!s.subject.equals(this.subject)) {
+			System.out.println(s.name +" has different subject!");
+			return;
+		}
+		if(freePlaces == 0) {
+			System.out.println("Sorry, there is not free place");
+			return;
+		}
+		
+		students[students.length - freePlaces] = s;
+		freePlaces --;
+	}
+	
+	void emptyGroup() {
+		freePlaces = 5;
+		students = new Student[5];
+	}
+	
+	String findBestStudent() {
+		// be careful of the group have students
+		Student bestStudent = students[0];
+		
+		for (int i = 1; i < students.length - freePlaces; i++) {
+			if(students[i].grade > bestStudent.grade) {
+				bestStudent = students[i];
+			}
+		}
+		return bestStudent.name;
+	}
+	
+	void printStudentsInGroup() {
+		for (int i = 0; i < students.length - freePlaces; i++) {
+			System.out.println("Name:" + students[i].name + " age: " + students[i].age );
+		}
+	}
+}
